4d6f21853af0aeeacbe3882323e534f5fc0ad92c
WeiXiaoSummer <wei.xiao.xia@est.fib.upc.edu>
2020-03-22 19:34:47 +0100

    error fixed

--- src/main.java
+++ src/main.java
@@ -11,26 +11,29 @@ import java.util.*;
 
 public class main {
     public static void main(String[] args) throws Exception {
-        long startTime=System.currentTimeMillis();
-        ServerState state = new ServerState(200, 5,1234, 50, 5, 1234);
-        state.initial_sol_2();
-        Problem problem = new Problem(state, new ServerSuccessorFunction(), new ServerGoalTest(), new ServerHeuristicFunction1());
-        Search search = new HillClimbingSearch();
-        SearchAgent agent = new SearchAgent(problem, search);
-        long endTime=System.currentTimeMillis();
-        System.out.println((endTime - startTime) * 0.001 + " execution time");
-        //printActions(agent.getActions());
-        System.out.println(state.getMaxServerTime() + " initial maxServer time");
-        System.out.println(state.get_total_time() + " initial total time");
-        printInstrumentation(agent.getInstrumentation());
-        System.out.println(((ServerState)search.getGoalState()).getMaxServerTime() + " result maxServer time");
-        System.out.println(((ServerState)search.getGoalState()).get_total_time() + " result total time");
-        System.out.println("\n");
-        /*state.initial_sol_2 ();
-        Problem problem2 = new Problem(state, new ServerSuccessorFunction2(), new ServerGoalTest(), new ServerHeuristicFunction2());
-        Search search2 = new SimulatedAnnealingSearch();
-        SearchAgent agent2 = new SearchAgent(problem2, search2);
-        System.out.println(((ServerState)search2.getGoalState()).get_total_time());*/
+        int[] parameter = {689, 1137, 322, 668, 316, 582, 1203, 1039, 1014, 239, 810,477, 196, 981, 894,300, 1173, 245, 404, 1093};
+        for (int i = 0; i < 19; i+=2) {
+            long startTime=System.currentTimeMillis();
+            ServerState state = new ServerState(200, 5, parameter[i], 50, 5, parameter[i+1]);
+            state.initial_sol_3();
+            Problem problem = new Problem(state, new ServerSuccessorFunction(), new ServerGoalTest(), new ServerHeuristicFunction1());
+            Search search = new HillClimbingSearch();
+            SearchAgent agent = new SearchAgent(problem, search);
+            long endTime=System.currentTimeMillis();
+            System.out.println((endTime - startTime) * 0.001 + " execution time");
+            //printActions(agent.getActions());
+            System.out.println(state.getMaxServerTime() + " initial maxServer time");
+            System.out.println(state.get_total_time() + " initial total time");
+            printInstrumentation(agent.getInstrumentation());
+            System.out.println(((ServerState)search.getGoalState()).getMaxServerTime() + " result maxServer time");
+            System.out.println(((ServerState)search.getGoalState()).get_total_time() + " result total time");
+            System.out.println("\n");
+            /*state.initial_sol_2 ();
+            Problem problem2 = new Problem(state, new ServerSuccessorFunction2(), new ServerGoalTest(), new ServerHeuristicFunction2());
+            Search search2 = new SimulatedAnnealingSearch();
+            SearchAgent agent2 = new SearchAgent(problem2, search2);
+            System.out.println(((ServerState)search2.getGoalState()).get_total_time());*/
+        }
     }
     private static void printInstrumentation(Properties properties) {
         Iterator keys = properties.keySet().iterator();
